954e37f228f394ac213a0c70c135e2ba7ca0b43f
strahd <jv.palha@gmail.com>
2025-01-08 16:53:05 -0300

    add: change member role use case and test

---
+++ src/main/java/com/victorpalha/aspop_spring/domain/member/useCases/ChangeMemberRoleUseCase.java
@@ -0,0 +1,45 @@
+package com.victorpalha.aspop_spring.domain.member.useCases;
+
+import com.victorpalha.aspop_spring.domain.member.constants.MemberRole;
+import com.victorpalha.aspop_spring.domain.member.entities.MemberEntity;
+import com.victorpalha.aspop_spring.domain.member.exceptions.MemberNotFoundError;
+import com.victorpalha.aspop_spring.domain.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * This use case change the member role by member ID
+ * If member is MEMBER will be changed to ADMIN
+ * If member is ADMIN will be changed to MEMBER
+ * @author Victor Palha
+ * @version 1.0
+ * @since 08/01/25
+ */
+@Service
+public class ChangeMemberRoleUseCase {
+
+    private final MemberRepository memberRepository;
+    public ChangeMemberRoleUseCase(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public MemberEntity execute(String memberId){
+        Optional<MemberEntity> memberExists = memberRepository.findById(memberId);
+        if (memberExists.isEmpty()) {
+            throw new MemberNotFoundError();
+        }
+        MemberEntity memberToUpdate = this.updateMemberRole(memberExists.get());
+        this.memberRepository.save(memberToUpdate);
+        return memberToUpdate;
+    }
+
+    private MemberEntity updateMemberRole(MemberEntity member) {
+        if(member.getRole() == MemberRole.MEMBER) {
+            member.setRole(MemberRole.ADMIN);
+            return member;
+        }
+        member.setRole(MemberRole.MEMBER);
+        return member;
+    }
+}
---
+++ src/test/java/com/victorpalha/aspop_spring/domain/member/useCases/ChangeMemberRoleUseCaseTest.java
@@ -0,0 +1,119 @@
+package com.victorpalha.aspop_spring.domain.member.useCases;
+
+import com.victorpalha.aspop_spring.domain.member.constants.MaritalStatus;
+import com.victorpalha.aspop_spring.domain.member.constants.MemberRole;
+import com.victorpalha.aspop_spring.domain.member.dtos.CreateMemberRequestDTO;
+import com.victorpalha.aspop_spring.domain.member.entities.MemberEntity;
+import com.victorpalha.aspop_spring.domain.member.exceptions.MemberNotFoundError;
+import com.victorpalha.aspop_spring.domain.member.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ChangeMemberRoleUseCaseTest {
+
+    @InjectMocks
+    private ChangeMemberRoleUseCase changeMemberRoleUseCase;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Test
+    public void should_change_member_role_to_admin(){
+        // Mock data
+        String memberId = "12345";
+        CreateMemberRequestDTO createMemberRequestDTO = CreateMemberRequestDTO.builder()
+                .memberName("Jane Doe")
+                .email("janedoe@example.com")
+                .cpf("12345678901")
+                .rg("9876543")
+                .birthDate(LocalDate.of(1985, 3, 20))
+                .motherName("Mary Doe")
+                .fatherName("John Doe")
+                .naturalness("California")
+                .maritalStatus(MaritalStatus.CASADO)
+                .formation("Master's in Business Administration")
+                .workLocation("Branch Office")
+                .numberRegistration("EMP654321")
+                .admissionDate(LocalDate.of(2018, 6, 15))
+                .address("456 Elm Street")
+                .addressNumber("789")
+                .cep("54321-123")
+                .phoneNumber("+1 987 654 3210")
+                .build();
+        MemberEntity memberEntity = createMemberRequestDTO.toEntity();
+        memberEntity.setMemberId(memberId);
+
+        when(memberRepository.findById("12345")).thenReturn(Optional.of(memberEntity));
+
+        try{
+            MemberEntity result = changeMemberRoleUseCase.execute(memberId);
+            assert (result != null);
+            assert (result.getMemberId().equals(memberId));
+            assert (result.getRole() == MemberRole.ADMIN);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void should_change_member_role_to_member(){
+        // Mock data
+        String memberId = "12345";
+        CreateMemberRequestDTO createMemberRequestDTO = CreateMemberRequestDTO.builder()
+                .memberName("Jane Doe")
+                .email("janedoe@example.com")
+                .cpf("12345678901")
+                .rg("9876543")
+                .birthDate(LocalDate.of(1985, 3, 20))
+                .motherName("Mary Doe")
+                .fatherName("John Doe")
+                .naturalness("California")
+                .maritalStatus(MaritalStatus.CASADO)
+                .formation("Master's in Business Administration")
+                .workLocation("Branch Office")
+                .numberRegistration("EMP654321")
+                .admissionDate(LocalDate.of(2018, 6, 15))
+                .address("456 Elm Street")
+                .addressNumber("789")
+                .cep("54321-123")
+                .phoneNumber("+1 987 654 3210")
+                .build();
+        MemberEntity memberEntity = createMemberRequestDTO.toEntity();
+        memberEntity.setMemberId(memberId);
+        memberEntity.setRole(MemberRole.ADMIN);
+
+        when(memberRepository.findById("12345")).thenReturn(Optional.of(memberEntity));
+
+        try{
+            MemberEntity result = changeMemberRoleUseCase.execute(memberId);
+            assert (result != null);
+            assert (result.getMemberId().equals(memberId));
+            assert (result.getRole() == MemberRole.MEMBER);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void should_not_change_member_role_if_member_does_not_exist(){
+        // Mock data
+        String memberId = "12345";
+
+        when(memberRepository.findById("12345")).thenReturn(Optional.empty());
+
+        try{
+            changeMemberRoleUseCase.execute(memberId);
+        } catch (Exception e) {
+            assert e instanceof MemberNotFoundError;
+        }
+    }
+}
