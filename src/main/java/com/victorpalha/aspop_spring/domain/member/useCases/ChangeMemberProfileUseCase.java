15a38b7d7fe6e85bf6096f53aff50991d86c6e18
strahd <jv.palha@gmail.com>
2025-01-08 18:33:05 -0300

    add: change member profile use case and test

---
+++ src/main/java/com/victorpalha/aspop_spring/domain/member/useCases/ChangeMemberProfileUseCase.java
@@ -0,0 +1,91 @@
+package com.victorpalha.aspop_spring.domain.member.useCases;
+
+import com.victorpalha.aspop_spring.domain.member.dtos.CreateMemberRequestDTO;
+import com.victorpalha.aspop_spring.domain.member.entities.MemberEntity;
+import com.victorpalha.aspop_spring.domain.member.exceptions.MemberNotFoundError;
+import com.victorpalha.aspop_spring.domain.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * This use case get information from member and changes on the database
+ * @author Victor Palha
+ * @version 1.0
+ * @since 08/01/25
+ */
+@Service
+public class ChangeMemberProfileUseCase {
+    private final MemberRepository memberRepository;
+
+    public ChangeMemberProfileUseCase(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public MemberEntity execute(String memberId, CreateMemberRequestDTO changeMemberRequestDTO) {
+        Optional<MemberEntity> memberExists = memberRepository.findById(memberId);
+        if (memberExists.isEmpty()) {
+            throw new MemberNotFoundError();
+        }
+        MemberEntity memberUpdated = this.updateMember(memberExists.get(), changeMemberRequestDTO);
+
+        memberRepository.save(memberUpdated);
+        return memberUpdated;
+    }
+
+    private MemberEntity updateMember(MemberEntity memberEntity, CreateMemberRequestDTO changeMemberRequestDTO) {
+        memberEntity.setMemberName(
+                changeMemberRequestDTO.getMemberName() != null ? changeMemberRequestDTO.getMemberName() : memberEntity.getMemberName()
+        );
+        memberEntity.setEmail(
+                changeMemberRequestDTO.getEmail() != null ? changeMemberRequestDTO.getEmail() : memberEntity.getEmail()
+        );
+        memberEntity.setCpf(
+                changeMemberRequestDTO.getCpf() != null ? changeMemberRequestDTO.getCpf() : memberEntity.getCpf()
+        );
+        memberEntity.setRg(
+                changeMemberRequestDTO.getRg() != null ? changeMemberRequestDTO.getRg() : memberEntity.getRg()
+        );
+        memberEntity.setBirthDate(
+                changeMemberRequestDTO.getBirthDate() != null ? changeMemberRequestDTO.getBirthDate() : memberEntity.getBirthDate()
+        );
+        memberEntity.setMotherName(
+                changeMemberRequestDTO.getMotherName() != null ? changeMemberRequestDTO.getMotherName() : memberEntity.getMotherName()
+        );
+        memberEntity.setFatherName(
+                changeMemberRequestDTO.getFatherName() != null ? changeMemberRequestDTO.getFatherName() : memberEntity.getFatherName()
+        );
+        memberEntity.setNaturalness(
+                changeMemberRequestDTO.getNaturalness() != null ? changeMemberRequestDTO.getNaturalness() : memberEntity.getNaturalness()
+        );
+        memberEntity.setMaritalStatus(
+                changeMemberRequestDTO.getMaritalStatus() != null ? changeMemberRequestDTO.getMaritalStatus() : memberEntity.getMaritalStatus()
+        );
+        memberEntity.setFormation(
+                changeMemberRequestDTO.getFormation() != null ? changeMemberRequestDTO.getFormation() : memberEntity.getFormation()
+        );
+        memberEntity.setAdmissionDate(
+                changeMemberRequestDTO.getAdmissionDate() != null ? changeMemberRequestDTO.getAdmissionDate() : memberEntity.getAdmissionDate()
+        );
+        memberEntity.setCep(
+                changeMemberRequestDTO.getCep() != null ? changeMemberRequestDTO.getCep() : memberEntity.getCep()
+        );
+        memberEntity.setAddress(
+                changeMemberRequestDTO.getAddress() != null ? changeMemberRequestDTO.getAddress() : memberEntity.getAddress()
+        );
+        memberEntity.setAddressNumber(
+                changeMemberRequestDTO.getAddressNumber() != null ? changeMemberRequestDTO.getAddressNumber() : memberEntity.getAddressNumber()
+        );
+        memberEntity.setPhoneNumber(
+                changeMemberRequestDTO.getPhoneNumber() != null ? changeMemberRequestDTO.getPhoneNumber() : memberEntity.getPhoneNumber()
+        );
+        memberEntity.setWorkLocation(
+                changeMemberRequestDTO.getWorkLocation() != null ? changeMemberRequestDTO.getWorkLocation() : memberEntity.getWorkLocation()
+        );
+        memberEntity.setNumberRegistration(
+                changeMemberRequestDTO.getNumberRegistration() != null ? changeMemberRequestDTO.getNumberRegistration() : memberEntity.getNumberRegistration()
+        );
+
+        return memberEntity;
+    }
+}
---
+++ src/test/java/com/victorpalha/aspop_spring/domain/member/useCases/ChangeMemberProfileUseCaseTest.java
@@ -0,0 +1,88 @@
+package com.victorpalha.aspop_spring.domain.member.useCases;
+
+import com.victorpalha.aspop_spring.domain.member.constants.MaritalStatus;
+import com.victorpalha.aspop_spring.domain.member.dtos.CreateMemberRequestDTO;
+import com.victorpalha.aspop_spring.domain.member.entities.MemberEntity;
+import com.victorpalha.aspop_spring.domain.member.exceptions.MemberNotFoundError;
+import com.victorpalha.aspop_spring.domain.member.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ChangeMemberProfileUseCaseTest {
+
+    @InjectMocks
+    private ChangeMemberProfileUseCase changeMemberProfileUseCase;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Test
+    public void should_update_member_profile() {
+        String memberId = "123";
+        CreateMemberRequestDTO changeMemberRequestDTO = CreateMemberRequestDTO.builder()
+                .memberName("Updated Name")
+                .email("updated@example.com")
+                .cpf("98765432100")
+                .rg("7654321")
+                .birthDate(LocalDate.of(1992, 7, 25))
+                .motherName("Updated Mother")
+                .fatherName("Updated Father")
+                .naturalness("Updated City")
+                .maritalStatus(MaritalStatus.DIVORCIADO)
+                .formation("Updated Degree")
+                .workLocation("Updated Location")
+                .admissionDate(LocalDate.of(2015, 4, 10))
+                .address("Updated Street")
+                .addressNumber("999")
+                .numberRegistration("123456789")
+                .cep("98765-432")
+                .phoneNumber("+1 987 654 3210")
+                .build();
+
+        MemberEntity existingMember = new MemberEntity();
+        existingMember.setMemberId(memberId);
+
+        when(memberRepository.findById(memberId)).thenReturn(Optional.of(existingMember));
+        when(memberRepository.save(any(MemberEntity.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        MemberEntity updatedMember = changeMemberProfileUseCase.execute(memberId, changeMemberRequestDTO);
+        System.out.println(updatedMember);
+        assert updatedMember != null;
+        assert updatedMember.getEmail().equals(changeMemberRequestDTO.getEmail());
+        assert updatedMember.getMemberName().equals(changeMemberRequestDTO.getMemberName());
+        assert updatedMember.getCpf().equals(changeMemberRequestDTO.getCpf());
+        assert updatedMember.getRg().equals(changeMemberRequestDTO.getRg());
+        assert updatedMember.getAddress().equals(changeMemberRequestDTO.getAddress());
+
+        verify(memberRepository, times(1)).findById(memberId);
+        verify(memberRepository, times(1)).save(updatedMember);
+    }
+
+    @Test
+    public void should_throw_exception_when_member_not_found() {
+        String memberId = "123";
+        CreateMemberRequestDTO changeMemberRequestDTO = CreateMemberRequestDTO.builder()
+                .memberName("Nonexistent Member")
+                .build();
+
+        when(memberRepository.findById(memberId)).thenReturn(Optional.empty());
+
+        try {
+            changeMemberProfileUseCase.execute(memberId, changeMemberRequestDTO);
+        } catch (Exception e) {
+            assert e instanceof MemberNotFoundError;
+        }
+
+        verify(memberRepository, times(1)).findById(memberId);
+        verify(memberRepository, never()).save(any(MemberEntity.class));
+    }
+}
